b8c99de93c546cd672376f534319f8e3aca7d3d2
Alejandro Camarillo <alejandro.camarillo@wizeline.com>
2020-10-10 12:07:24 -0500

    Chore: linting

--- src/Styles.tsx
+++ src/Styles.tsx
@@ -103,11 +103,11 @@ export const StyledLink = styled.a`
 `;
 
 export const StyledFooter = styled.footer`
-    background: #161a1f;
-    left: 0;
-    right: 0;
-    padding: 1em;
-    position: fixed;
-    bottom: 0;
-    margin-top: 1em;
+  background: #161a1f;
+  left: 0;
+  right: 0;
+  padding: 1em;
+  position: fixed;
+  bottom: 0;
+  margin-top: 1em;
 `;
