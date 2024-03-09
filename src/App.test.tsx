2210843d939bc9f28deffef43728f266a1801cb0
Ale Camarillo <g.camarillo@traxion.global>
2024-03-09 04:10:05 -0600

    chore: Adds test

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { test, expect } from 'vitest';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders app title', () => {
+  const { getByText } = render(<App />);
+  const titleElement = getByText(/GEAAS/i);
+  expect(titleElement).toBeInTheDocument();
+});
